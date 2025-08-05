02e4c5ab00d84337f4f6bd8c6a64567d6691a1ec
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-08-05 23:28:23 +0200

    Modifications and testing Parameterized Tests

--- src/FactorialTest.java
+++ src/FactorialTest.java
@@ -1,4 +1,8 @@
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,5 +34,39 @@ public class FactorialTest {
     @Test
     public void testFactorialOf4() {
         assertEquals(24, factorial.compute(4));
+    }
+}
+@RunWith(Parameterized.class)
+public class FactorialParameterizedTest {
+
+    private int input;
+    private int expected;
+    private Factorial factorial;
+
+    public FactorialParameterizedTest(int input, int expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Before
+    public void setup() {
+        factorial = new Factorial();
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][] {
+            {0, 1},
+            {1, 1},
+            {2, 2},
+            {3, 6},
+            {4, 24},
+            {5, 120}
+        };
+    }
+
+    @Test
+    public void testCompute() {
+        assertEquals(expected, factorial.compute(input));
     }
 }
